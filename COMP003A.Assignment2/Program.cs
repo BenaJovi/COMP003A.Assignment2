9079bcd7787930949d6213548d6b9768eaf6121e
BenaJovi <xavibenavides@yahoo.com>
2023-01-23 22:30:44 -0800

    Add project files.

---
+++ COMP003A.Assignment2/Program.cs
@@ -0,0 +1,40 @@
+﻿/*  Author: Jovani Benavides
+ *  Course: Comp-003A
+ *  Purpose: Lecture assignment1 100 things about me 
+ */
+
+namespace COMP003A.Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("*********************************");
+            Console.WriteLine("String Section");
+            Console.WriteLine("*********************************");
+            Console.Write("Enter First Name:");
+            string FirstName= Console.ReadLine();
+            Console.Write("Enter Middle Name:");
+            string MiddleName= Console.ReadLine();
+            Console.Write("Enter Last Name:");
+            string LastName= Console.ReadLine();
+            Console.Write("Enter Age In 2023:");
+            string InputAge = Console.ReadLine();
+            int YearBorn = 2023 - Convert.ToInt32(InputAge);
+           Console.WriteLine($"Hello, {FirstName} {MiddleName} {LastName}. You were born in {YearBorn}.");
+            */
+            Console.WriteLine("*********************************");
+            Console.WriteLine("Math Section");
+            Console.WriteLine("*********************************");
+            int Integer1;
+            int Integer2;
+            string userinput;
+            Console.Write("Enter an Integer for Integer1:");
+            userinput = Console.ReadLine();
+            
+            
+
+        }
+
+    }
+}
