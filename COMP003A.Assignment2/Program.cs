2ac34f0a7c13715add4fc12a0fde6348e4b46023
Jovani Benavides <xavibenavides@yahoo.com>
2023-01-25 19:13:49 -0800

    TEST

--- COMP003A.Assignment2/Program.cs
+++ COMP003A.Assignment2/Program.cs
@@ -9,50 +9,57 @@ namespace COMP003A.Assignment2
     {
         static void Main(string[] args)
         {
-            /*Console.WriteLine("*********************************");
+            Console.Title = "COMP-003A - Assignment 2";
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("*********************************");
             Console.WriteLine("String Section");
             Console.WriteLine("*********************************");
             Console.Write("Enter First Name:");
-            string FirstName= Console.ReadLine();
+            string FirstName = Console.ReadLine();
             Console.Write("Enter Middle Name:");
-            string MiddleName= Console.ReadLine();
+            string MiddleName = Console.ReadLine();
             Console.Write("Enter Last Name:");
-            string LastName= Console.ReadLine();
+            string LastName = Console.ReadLine();
             Console.Write("Enter Age In 2023:");
             string InputAge = Console.ReadLine();
             int YearBorn = 2023 - Convert.ToInt32(InputAge);
-           Console.WriteLine($"Hello, {FirstName} {MiddleName} {LastName}. You were born in {YearBorn}.");
-            */
+            Console.WriteLine($"Hello, {FirstName} {MiddleName} {LastName}. You were born in {YearBorn}.");
+
             Console.WriteLine("*********************************");
             Console.WriteLine("Math Section");
             Console.WriteLine("*********************************");
-        
-            
-            string userinput;
-            Console.Write("Enter an Integer for Integer1:");
-            string userinput = console.readline();
-            int Integer1= Convert
-            Console.Write("Enter an Integer for Integer2:");
-            userinput = Console.ReadLine();
-            Console.Writeline($"{Integer1} + {Integer2} = {Integer1 + Integer2}");
-            Console.Writeline($"{Integer1} - {Integer2} = {Integer1 - Integer2}");
-            Console.Writeline($"{Integer1} * {Integer2} = {Integer1 * Integer2}");
-            Console.Writeline($"{Integer1} / {Integer2} = {Integer1 / Integer2}");
-            Console.Writeline($"{Integer1} % {Integer2} = {Integer1 + Integer2}");
-            
-            /*Console.WriteLine("*********************************");
+
+            Console.Write("Enter an number for Integer1:");
+            string integerone = Console.ReadLine();
+            int integer1 = Convert.ToInt32(integerone);
+            Console.Write("Enter an number for Integer2:");
+            string integertwo = Console.ReadLine();
+            int integer2 = Convert.ToInt32(integertwo);
+
+            Console.WriteLine($"{integer1} + {integer2} = {integer1 + integer2}");
+            Console.WriteLine($"{integer1} - {integer2} = {integer1 - integer2}");
+            Console.WriteLine($"{integer1} * {integer2} = {integer1 * integer2}");
+            Console.WriteLine($"{integer1} / {integer2} = {integer1 / integer2}");
+            Console.WriteLine($"{integer1} % {integer2} = {integer1 % integer2}");
+
+            Console.WriteLine("*********************************");
             Console.WriteLine("Circle Area & Circumfrence Calculator Section");
             Console.WriteLine("*********************************");
-            
-            int Radius;
-            string userinput2;
+
             Console.Write("Enter the radius:");
-            userinput2 = Console.ReadLine();
-            Console.ReadLine($"{Radius
-            
-            
-         
-            
+            string inputradius = Console.ReadLine();
+            double radius = Convert.ToDouble(inputradius);
+            double area = Math.PI * Math.Pow(radius, 2);
+            Console.Write("The area is: ");
+            Console.WriteLine(area);
+            Console.Write("The circumference is: ");
+            double circumference = 2 * Math.PI * radius;
+            Console.WriteLine(circumference);
+
+
+
+
 
         }
 
